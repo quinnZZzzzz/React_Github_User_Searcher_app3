78d9a36152544436eac630d91fbca9e0ff7428ab
QianZuo <quinn.zuoq@gmail.com>
2022-03-10 11:46:16 +0000

    Create user folder  and context folder - useContext

--- src/App.js
+++ src/App.js
@@ -4,23 +4,26 @@ import NotFound from "./pages/NotFound";
 import About from "./pages/About";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
+import { GithubProvider } from "./context/github/GithubContext";
 
 function App() {
   return (
-    <Router>
-      <div className="flex flex-col justify-between h-screen">
-        <Navbar />
-        <main className="container mx-auto p3-3 pb12">
-          <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/about" element={<About />}></Route>
-            <Route path="/notfound" element={<NotFound />}></Route>
-            <Route path="/*" element={<NotFound />}></Route>
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <GithubProvider>
+      <Router>
+        <div className="flex flex-col justify-between h-screen">
+          <Navbar />
+          <main className="container mx-auto p3-3 pb12">
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/about" element={<About />}></Route>
+              <Route path="/notfound" element={<NotFound />}></Route>
+              <Route path="/*" element={<NotFound />}></Route>
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </GithubProvider>
   );
 }
 

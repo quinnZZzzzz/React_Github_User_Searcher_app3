a4938cdf61ee2ddd3176f66b59d750bc2666c8dc
QianZuo <quinn.zuoq@gmail.com>
2022-03-10 12:02:09 +0000

    Create GihubReducer to manage states (useReducer instead of useState)

---
+++ src/context/github/GithubContext.js
@@ -0,0 +1,45 @@
+import { useReducer, createContext, useEffect } from "react";
+import { useState } from "react";
+import githubReducer from "./GithubReducer";
+
+const GithubContext = createContext();
+
+const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+
+export const GithubProvider = ({ children }) => {
+  // const [users, setUsers] = useState([]);
+  // const [loading, setLoading] = useState(true);
+
+  const initialState = {
+    users: [],
+    loading: true,
+  };
+
+  const [state, dispatch] = useReducer(githubReducer, initialState);
+
+  const fetchUsers = async () => {
+    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`);
+    const data = await response.json();
+    // setUsers(data);
+    // setLoading(false);
+    dispatch({
+      type: "GET_USERS",
+      payload: data, // sending data as payload
+    });
+  };
+
+  return (
+    <GithubContext.Provider
+      value={{
+        users: state.users,
+        loading: state.loading,
+        fetchUsers,
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  );
+};
+
+export default GithubContext;
